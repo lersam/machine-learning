607457eb847023f85018b281ac7e06767011f3d9
Samuel Lerner <lersam@gmail.com>
2026-05-14 13:52:00 +0300

    feat: Update chat model initialization and add response handling functions

--- lang_chain/stand_alone.py
+++ lang_chain/stand_alone.py
@@ -8,9 +8,31 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 model = init_chat_model(
-    model="ollama:qwen3.5:0.8b",
+    model="ollama:gemma3:4b-it-qat",
     temperature=0.1,
 )
+
+def get_model_response(conversation):
+    try:
+        response = model.invoke(conversation)
+        logger.info("----------------")
+        logger.info(response)
+        logger.info("----------------")
+        logger.info("content %s", response.content) 
+        return response
+    except httpx.HTTPError as e:
+        logger.error("HTTP error occurred: %s", e)
+    except Exception as e:
+        logger.error("An error occurred: %s", e)
+
+def get_model_streaming_response(conversation):
+    try:
+        for chunk in model.stream(conversation):
+            print(chunk.text, end="", flush=True)
+    except httpx.HTTPError as e:
+        logger.error("HTTP error occurred: %s", e)
+    except Exception as e:
+        logger.error("An error occurred: %s", e)
 
 if __name__ == "__main__":
     conversation = [
@@ -19,8 +41,5 @@ if __name__ == "__main__":
         AIMessage(content="Python is a high-level, interpreted programming language known for its simplicity and readability. It is widely used for web development, data analysis, artificial intelligence, scientific computing, and more."),
         HumanMessage(content="When was it released?"),
     ]
-    response = model.invoke(conversation)
-    logger.info("----------------")
-    logger.info(response)
-    logger.info("----------------")
-    logger.info("content %s", response.content)
+
+    get_model_streaming_response(conversation)
