26a683d9b397ccafb329c5499dddac11e9bfdc3e
Samuel Lerner <LerSam@gmail.com>
2026-07-09 23:29:19 +0300

    Refactor code structure for improved readability and maintainability

---
+++ langchain-academy/lca-lc-foundations/openai_example.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+load_dotenv()
+
+client = OpenAI(
+  api_key=os.getenv("OPENAI_API_KEY")
+)
+
+response = client.responses.create(
+  model="gpt-5.4-mini",
+  input="what is the capital of Israel?",
+  store=True,
+)
+
+print(response.output_text);
